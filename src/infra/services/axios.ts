7715bf5f8b198673316f7194df9a4e32ceaf00ec
Samuel Paiva <sampaiva97@gmail.com>
2024-03-21 00:17:34 +0000

    removing api URL

--- src/infra/services/axios.ts
+++ src/infra/services/axios.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { checkifUserIsAuthenticated, getJwtToken } from './auth/authService';
 
-console.log(import.meta.env.VITE_API_URL);
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
